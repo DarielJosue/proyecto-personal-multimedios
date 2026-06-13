e4966e259729a6590449304103b293202bc96cdb
DarielJosue <dariel.benavides@ucr.ac.cr>
2026-06-12 18:31:41 -0600

    Se crean componentes base y configura estilos y variables  globales. Integra página home con imports de componentes.

---
+++ src/app/components/info-section/info-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-section',
+  imports: [],
+  templateUrl: './info-section.html',
+  styleUrl: './info-section.css',
+})
+export class InfoSection {}
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { Hero } from '../../components/hero/hero';
+import { InfoSection } from '../../components/info-section/info-section';
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
